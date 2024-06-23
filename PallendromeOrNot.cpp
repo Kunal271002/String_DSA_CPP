f946a53e68e2fbc2d05b8f84e655f2e1fe5851c0
Kunal Sharma <kk.s67533@gmail.com>
2024-06-23 20:42:18 +0530

    Pallendrome

---
+++ PallendromeOrNot.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+void Reverse(string &s){
+    int start = 0;
+    int end = s.size()-1;
+    while(start<end){
+        if(s[start] == s[end]){
+            start++;
+            end--;
+        }
+        else{
+            cout<<"It is not an Pallendrome";
+            return;
+        }
+    }
+    cout<<"It is a Pallendrome";
+    
+}
+
+
+
+int main(){
+    string s;
+    getline(cin,s);
+    Reverse(s); 
+}
